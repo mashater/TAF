0227110dc03ea412f270017c17b916ee963db0f7
terenyamasha <terenyamasha@gmail.com>
2022-04-13 21:23:44 +0300

    deleted all

--- src/main/java/Listener.java
+++
@@ -1,9 +0,0 @@
-import org.testng.ITestListener;
-
-public class Listener implements ITestListener {
-    public void onTestFailure(org.testng.ITestResult result) {
-        System.out.println("Make a screenshot ");
-    }
-
-
-}
