94cc29c0373c1e5e90be0de741ee6b0c41bce433
terenyamasha <terenyamasha@gmail.com>
2022-04-22 13:51:29 +0300

    .

--- src/main/java/SimpleDriver.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class SimpleDriver {
-    public WebDriver getDriver(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\m.terenya\\IdeaProjects\\TAF\\src\\test\\resources\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-    }
-}
