10cb477167b16899d6712bbc15479dd28039ab3c
terenyamasha <terenyamasha@gmail.com>
2022-04-22 14:15:44 +0300

    .

---
+++ src/main/java/services/SimpleDriver.java
@@ -0,0 +1,12 @@
+package services;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SimpleDriver {
+    public WebDriver getDriver(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\m.terenya\\IdeaProjects\\TAF\\src\\test\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        return new ChromeDriver();
+    }
+}
