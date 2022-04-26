25e5efe8c91df73627a7ada8349dda586dd6cd99
terenyamasha <terenyamasha@gmail.com>
2022-04-26 15:24:42 +0300

    HomeWork

--- src/test/java/tests/Homework_laminate.java
+++ src/test/java/tests/Homework_laminate.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -30,8 +31,39 @@ public class Homework_laminate {
             selectMethod.selectByIndex(2);
             Thread.sleep(2000);
 
-            driver.findElement(By.id("ln_room_id")).sendKeys("500");
-            driver.findElement(By.id("wd_room_id")).sendKeys("400");
+            WebElement lengthRoom = driver.findElement(By.id("ln_room_id"));
+            lengthRoom.click();
+            lengthRoom.clear();
+            lengthRoom.sendKeys("500");
+
+            WebElement widthRoom = driver.findElement(By.id("wd_room_id"));
+            widthRoom.click();
+            widthRoom.clear();
+            widthRoom.sendKeys("400");
+
+            WebElement lengthLaminate = driver.findElement(By.id("ln_lam_id"));
+            lengthLaminate.click();
+            lengthLaminate.clear();
+            lengthLaminate.sendKeys("2000");
+
+            WebElement widthLaminate = driver.findElement(By.id("wd_lam_id"));
+            widthLaminate.click();
+            widthLaminate.clear();
+            widthLaminate.sendKeys("200");
+            Thread.sleep(2000);
+
+            driver.findElement(By.id("direction-laminate-id1")).click();
+            driver.findElement(By.cssSelector("div.calc-btn-div>a.calc-btn")).click();
+            Thread.sleep(2000);
+
+            WebElement amountLaminate = driver.findElement(By.cssSelector("div.calc-footer>div>div>span[style = 'padding:5px 0;font-size:22px; color:#C80303; font-weight:bold;']"));
+            Assert.assertEquals(amountLaminate.getText(), "53");
+
+            WebElement amountPackages = driver.findElement(By.cssSelector("div.calc-footer>div>div>span[style = 'padding:5px 0;font-size:18px; color:#0E8C19; font-weight:bold;']"));
+            Assert.assertEquals(amountPackages.getText(), "7");
+
+            Thread.sleep(2000);
+
 
         }
 }
