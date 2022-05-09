35331aed9c798833b14f7677af5fd2fdeb1dbba1
terenyamasha <terenyamasha@gmail.com>
2022-05-09 18:04:09 +0300

    Task_6 homework

--- src/test/java/tests/Task_6_Saucedemo.java
+++ src/test/java/tests/Task_6_Saucedemo.java
@@ -1,10 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import services.BrowsersService;
+
+import static org.openqa.selenium.By.cssSelector;
 
 public class Task_6_Saucedemo {
     private WebDriver driver;
@@ -25,19 +29,18 @@ public class Task_6_Saucedemo {
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user"); //id
         driver.findElement(By.name("password")).sendKeys("secret_sauce");  //name
-        driver.findElement(By.cssSelector("#login-button")).click();//#id
+        driver.findElement(cssSelector("#login-button")).click();//#id
         Thread.sleep(4000);
 
 
-        driver.findElement(By.cssSelector("#add-to-cart-sauce-labs-bike-light")).click();
+        driver.findElement(cssSelector("#add-to-cart-sauce-labs-bike-light")).click();
         driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click(); //поиск по атрибуту
         Thread.sleep(2000);
 
-
-        //.class
-        Assert.assertEquals(driver.findElement(By.cssSelector(".inventory_item_name")).getText(), "Sauce Labs Bike Light", "Wrong item name");
-        Assert.assertEquals(driver.findElement(By.cssSelector(".inventory_item_price")).getText(), "$9.99", "Wrong item price");
-        driver.findElement(By.cssSelector(".btn.btn_secondary.back.btn_medium")).click(); //.class.class....
+        // поиск по .class
+        Assert.assertEquals(driver.findElement(cssSelector(".inventory_item_name")).getText(), "Sauce Labs Bike Light", "Wrong item name");
+        Assert.assertEquals(driver.findElement(cssSelector(".inventory_item_price")).getText(), "$9.99", "Wrong item price");
+        driver.findElement(cssSelector(".btn.btn_secondary.back.btn_medium")).click(); //.class.class....
 
         Thread.sleep(4000);
 
@@ -56,13 +59,26 @@ public class Task_6_Saucedemo {
         Assert.assertTrue(driver.findElement(By.xpath("//div[@class='inventory_item_name'] | //div[@class='inventory_item_price']")).isDisplayed());
 
 
-
+        Assert.assertTrue(driver.findElement(By.cssSelector("select")).isDisplayed()); //tagname
+        Assert.assertTrue(driver.findElement(By.cssSelector("select.product_sort_container")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("select.product_sort_container")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[class='peek']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[alt~='Backpack']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("[class|='title']")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[class^=inventory]")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[class$=container]")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("div[class*='inve']")).isDisplayed());
 
     }
+    @Test
+    public void relativeLocators() throws InterruptedException {
+        driver.get("https://www.saucedemo.com/inventory.html");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user"); //id
+        driver.findElement(By.name("password")).sendKeys("secret_sauce");  //name
+        driver.findElement(cssSelector("#login-button")).click();//#id
+
+        WebElement element = driver.findElement(RelativeLocator.with(By.cssSelector(".inventory_item_name")).below(By.id("item_4_title_link")).toLeftOf(By.id("item_5_title_link")));
+        Assert.assertEquals(element.getText(), "Sauce Labs Bolt T-Shirt");
 
 
-
-
-
-}
-//By.xpath("//div[text()='Sauce Labs Bike Light']")
+}}
