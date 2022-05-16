be30ccd693c1ab178f3d9eb873f1d8cec822225f
terenyamasha <terenyamasha@gmail.com>
2022-05-16 21:09:15 +0300

    .

--- src/test/java/BaseEntities/BaseTest.java
+++ src/test/java/BaseEntities/BaseTest.java
@@ -1,17 +1,34 @@
 package BaseEntities;
 
+import com.google.gson.Gson;
 import configurations.ReadProperties;
+import models.Project;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import services.BrowsersService;
 import Steps.LoginStep;
 import Steps.NavigationStep;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class BaseTest {
     protected WebDriver driver;
     protected LoginStep loginStep;
     protected NavigationStep navigationStep;
+
+
+    protected Project mainProject;
+
+    @BeforeTest
+    public void prepareData() throws IOException {
+        Reader reader = Files.newBufferedReader(Paths.get("projectResult.json"));
+        mainProject = new Gson().fromJson(reader, Project.class);
+    }
 
     @BeforeMethod
     public void setup() {
---
+++ src/test/java/tests/ProjectTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import BaseEntities.BaseTest;
+import org.testng.annotations.Test;
+
+public class ProjectTest extends BaseTest {
+
+    @Test
+    public void addProjectTest(){
+        System.out.println(mainProject.toString());
+    }
+}
