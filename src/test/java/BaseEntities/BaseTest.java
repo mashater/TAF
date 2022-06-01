fb0b6cf708dacd99147dd2f9995778b579c96c35
terenyamasha <terenyamasha@gmail.com>
2022-06-01 20:52:58 +0300

    .

--- src/test/java/BaseEntities/BaseTest.java
+++ src/test/java/BaseEntities/BaseTest.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import configurations.ReadProperties;
 import models.Project;
 import org.openqa.selenium.WebDriver;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -32,6 +33,8 @@ public class BaseTest {
     public void prepareData() throws IOException {
         Reader reader = Files.newBufferedReader(Paths.get("projectResult.json"));
         mainProject = new Gson().fromJson(reader, Project.class);
+
+
     }
 
     @BeforeMethod
